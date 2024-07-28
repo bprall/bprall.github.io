553c675715b4b228a041d0b20a3fbdd51e00110d
bprall <brprall3@gmail.com>
2024-07-28 15:27:11 -0500

    adjust projects page and its styling

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -44,24 +44,25 @@ const RenderProjects: React.FC = () => {
         </div>
         {filteredProjects.map((project) => (
           <div key={project.id} className="projects-individual staggered-item">
-            <h4 className="projects-title" dangerouslySetInnerHTML={{ __html: project.title || '' }}/>
-            <div className="projects-desc-wrapper">
-              <div className='projects-desc-box'>
-                <div className="projects-desc-float">
-                  {project.descriptionImage && (
-                    <img
-                      src={project.descriptionImage}
-                      alt={project.title}
-                      className="projects-desc-image"
-                    />
+            <div className="home-card" key={project.id}>
+              <div className="home-card-content">
+                <h2 className="home-card-title">{project.title}</h2>
+                <p className="home-card-description">{project.frontPageDesc || project.description}</p>
+                <div className="home-card-buttons">
+                  <Link to={`/projects/${project.id}`} className="home-card-button">Page</Link>
+                  {project.titleLink && (
+                    <a href={project.titleLink} className="home-card-button" target="_blank" rel="noopener noreferrer">
+                    Source
+                    </a>
+                  )}
+                  {project.siteLink && (
+                    <a href={project.siteLink} className="home-card-button" target="_blank" rel="noopener noreferrer">
+                    Visit
+                    </a>
                   )}
                 </div>
-                <p className="projects-desc-text" dangerouslySetInnerHTML={{ __html: project.description || '' }} />
               </div>
             </div>
-            <Link to={`/projects/${project.id}`} className="read-more">
-              <button className="read-more-button">Read More</button>
-            </Link>
           </div>
         ))}
       </div>
